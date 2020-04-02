e9a6c130c2b4c4d9d5861eb99aeae3b5e25a64db
michelleandersson <michelleandersson@live.com>
2020-04-02 11:24:27 +0200

    complete lesson 1.1-1.6 except 1.5

---
+++ lesson-one/lesson-one.js
@@ -0,0 +1,60 @@
+//1. Request a length of a side of a square from a user and display the perimeter of the square in the console.
+
+/*let side = prompt('Please provide the length of a side of a square');
+
+console.log(`The perimeter of your square is: ${side * 4}`); */
+
+
+
+//2. Request a user’s year of birth, count their age, and display the result to the console. Store the current year as a constant.
+
+/*let birth = prompt('Please provide your year of birth');
+let year = 2020; 
+
+console.log(`Your age is: ${year-birth}`); */
+
+
+
+/*3. Request a distance in kilometers between the two cities from a user, and the time they want to cover it within. Count the speed needed 
+    to be on time.*/
+
+/*let distance = prompt('How far away is your destination in KM?');
+let time = prompt('How many hours do you have to get there?');
+
+console.log(`You have to drive ${distance/time} KM per hour to get to your destination on time`); */
+
+
+
+/*4. Create a simple currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a
+    constant. Then display the result in the console.*/
+
+/*let us = prompt('How much money do you have in US$?');
+const eur = 0.95; 
+
+console.log(`You have ${us*eur} Euros`); */
+
+    
+
+//5. A user types in the values of a and b for the formula a * x + b = 0 . The program has to calculate the x and display it in the console.
+
+let a = prompt('Please provide a value for a');
+let b = prompt('Please provide a value for b');
+let x1 = parse("a * x - b"); 
+let x2 = parse("0"); 
+let eq = new Equation(x1,x2); 
+let answer = eq.solveFor("x"); 
+
+console.log("The value for X in the equation a*x+b=0 is:" + answer.toString()); 
+
+
+
+/*6. Request current time from a user in format HH:mm (HH is hour in a day from 00 to 23, mm are minutes in hour) and display how many hours 
+    and minutes are left until tomorrow.*/
+
+    /*let HH = prompt('What time is it in hours?');
+    let mm = prompt('What time is it in minutes?');
+
+    let hours = 24; 
+    let minutes = 59; 
+    
+    console.log(`Time left until tomorrow is: ${hours-HH} hours and ${minutes-mm} minutes`); */
