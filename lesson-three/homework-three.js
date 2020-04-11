3937a0722385d3fdb6d555ce92f2d6ef4ffa84cc
michelleandersson <michelleandersson@live.com>
2020-04-11 13:49:13 +0200

    finish homework 3.1

---
+++ lesson-three/homework-three.js
@@ -0,0 +1,25 @@
+/*1. Create a function that takes an integer number as an argument and returns
+ “Even” for even numbers or “Odd” for odd numbers.*/
+
+/* 
+function number (n) {
+    if (n % 2 === 0) {
+        return 'Even'; 
+    } else {
+        return 'Odd'
+    }
+}
+
+console.log(`Your number is ${number(55)}`); 
+*/
+
+/*2. Trolls are attacking your comment section! Create a function that takes a 
+string and returns a new string with all vowels removed. For example, 
+“This website is for losers LOL!” returns “Ths wbst s fr lsrs LL!“.*/
+
+
+
+/*3. Write a function which repeats the given string exact number of times. 
+For example, repeatString("Hello", 5) returns “HelloHelloHelloHelloHello”.*/
+
+
