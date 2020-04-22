9f29ad561196798f4398c4b17888409d84627585
michelleandersson <michelleandersson@live.com>
2020-04-22 20:23:21 +0200

    finish classwork 6.1-6.4. partway through 6.5

--- lesson-six/classwork-six.js
+++ lesson-six/classwork-six.js
@@ -6,21 +6,24 @@ displayed using the font properties of the class.
 Create an instance of that class and call the 
 print() method.*/
 
-/* NOT SOLVED!
+
 class PrintMachine {
     constructor(fontSize, fontColor, fontFamily, message){
         this.fontSize = fontSize; 
         this.fontColor = fontColor; 
         this.fontFamily = fontFamily; 
+        this.message = message; 
     }
-    print() {
-        document.write(this.message); 
+    printMessage() {
+        document.write(`<p style="font-size:${this.fontSize}px; font-family: ${this.fontFamily}; color:${this.fontColor};">Here is your message: ${this.message}</p>`); 
     }
 }
 
-let firstPrint = new PrintMachine(16, 'red', 'areal', 'Hello World'); 
-console.log(firstPrint); 
-*/
+ 
+
+let firstPrint = new PrintMachine(25, 'red', 'arial', 'Hello World'); 
+//console.log(firstPrint.printMessage()); 
+
 
 
 /*2. Create a Shape class that consists of name, 
@@ -66,22 +69,19 @@ automatically set to 4. When invoking the
 constructor, you should therefore just need to 
 provide the side length property.*/
 
-/* NOT FULLY SOLVED
+
 class Square extends Shape {
-    constructor(name, sideNumber, sideLength){
-        super(name, sideNumber, sideLength); 
-        this.name = 'Square'; 
-        this.sideNumber = 4; 
+    constructor(sideLength){
+        super('Square', 4, sideLength);  
     }
     calculateArea(){
-        return this.sideNumber * this.sideLength; 
+        return this.sideLength *  this.sideLength; 
     }
 }; 
 
 let firstSquare = new Square(10); 
-console.log(firstSquare); 
-console.log(firstSquare.calculateArea); 
-*/
+//console.log(firstSquare); 
+//console.log(firstSquare.calculateArea()); 
 
 
 
@@ -93,7 +93,7 @@ can eat. The eat() method should display a string
  that just displays some message. Create an instance 
  of Bird classand call eat() and fly() methods of it.*/
 
-/* 
+
  class Animal {
      constructor(name, food){
          this.name = name; 
@@ -105,7 +105,7 @@ can eat. The eat() method should display a string
  }; 
 
  let cat = new Animal ('cat', 'mice'); 
- console.log(cat.eat()); 
+ //console.log(cat.eat()); 
 
 class Bird extends Animal {
     constructor(name, food, wings){
@@ -124,6 +124,38 @@ class Bird extends Animal {
 
  let dove = new Bird('Dove', 'mice', 2); 
 
- console.log(dove.eat()); 
- console.log(dove.fly()); 
- */
+ //console.log(dove.eat()); 
+ //console.log(dove.fly()); 
+
+/*5. Create a class that describes an article 
+(heading, short description, tags array, date of 
+publication in format YYYY-MM-DD). Create a 
+method of the class that will print all the 
+article information to the webpage using
+document.write(). If the date of publication 
+was less than a day ago, it is displayed as
+“today”, if less than a week ago - “N days ago”,
+and otherwise it is shown as the full date format 
+dd:mm:yyyy.*/
+
+class Article {
+    constructor(heading, description, tags, date){
+        this.heading = heading; 
+        this.description = description; 
+        this.tags = tags; 
+        this.date = date;        
+    }
+
+    printArticle(){
+        document.write(`<h1>${this.heading}</h1>`);
+        document.write(`<h2>${this.description}</h2>`);
+        document.write(`<h4>${this.tags}</h4>`);
+        document.write(`<h4>Published: ${this.date}`);    
+    }
+}; 
+
+let firstArticle = new Article ('My Article', "A short article written by me.",
+['article', 'me', 'first'], 2020-04-18); 
+
+console.log(firstArticle.printArticle()); 
+ 
