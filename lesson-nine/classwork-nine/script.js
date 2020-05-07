141f555763760b83019dd45f2662c2200a887973
michelleandersson <michelleandersson@live.com>
2020-05-07 13:05:45 +0200

    finish classwork nine

--- lesson-nine/classwork-nine/script.js
+++ lesson-nine/classwork-nine/script.js
@@ -1,21 +1,11 @@
+$.ajax('employees.json').done((employees)=> {
+    let list = $('<ul></ul>'); 
+    list.addClass('bulleted'); 
 
-$.ajax('employees.json').done(function (response) {
-    
-
-    for (let employee of response) {
-        if ('employee.inOffice' == true){
-            $("#employeeList").addClass("in"); 
-        } else {
-            $("#employeeList").addClass("out");
-        };
-    let container = $('<div></div>');
-     
-    container.append(`
-    <ul>${employee.name}</ul>
-    `);
-    
-    $('#employeeList').append(container);
+    for ( let employee of employees) {
+        let listItem = $('<li></li>'); 
+        listItem.text(employee.name).addClass(employee.inOffice ? 'in' : 'out'); 
+        list.append(listItem); 
     }
-    
-    
-});
+    $('#employeeList').append(list); 
+})
