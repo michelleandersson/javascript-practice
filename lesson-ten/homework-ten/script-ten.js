2dff81f99380d0353fa73f1ae7ff72d3ec29d811
michelleandersson <michelleandersson@live.com>
2020-05-10 16:23:14 +0200

    finish homework ten

--- lesson-ten/homework-ten/script-ten.js
+++ lesson-ten/homework-ten/script-ten.js
@@ -1,38 +1,29 @@
 //1. add items from form to localstorage array
 //2. add items from local storage array to page 
 
-let songs = ['song 1', 'song 2', 'song 3', 'song 4'];
-localStorage.setItem('songs', JSON.stringify(songs));
-// Get songs array from the local storage, and parse the JSON data
-let song = JSON.parse(localStorage.getItem('songs'));
 
-let list = $('<ul></ul>'); 
-for (let allSongs of song){
-    let taskItem = $('<li></li>'); 
-    taskItem.text(allSongs); 
-    list.append(taskItem); 
+let taskArray = localStorage.getItem("taskArray");
+taskArray = (taskArray) ? JSON.parse(taskArray) : [];
+
+
+function clearF() {
+    window.localStorage.clear();
+    location.reload();
 }
-$('#tasks').append(list); 
+function addTask() {
+    var x = document.getElementById("taskInput").value;
+    taskArray.push(x);
+    localStorage.setItem("taskArray", JSON.stringify(taskArray));
+    let taskList = JSON.parse(localStorage.getItem("taskArray")); 
+
+    //THIS PRINTING IS NOT WORKING HOW I WANT IT TO :( 
+    document.getElementById("result").innerHTML=(`<ul><li>${taskList}</li></ul>`); 
+}
 
 
 
-let x = 0;
-let array = Array();
-let e;  
 
-function display_task(){
-    array[x] = document.getElementById("task").value;
-    x++;
-    document.getElementById("task").value = "";
-    e = "<hr/>";   
+
+ 
+
     
-   for (var i=0; i<array.length; i++){
-        e += array[i] + "<br/>";
-   }
-   document.getElementById("Result").innerHTML = e;
-}
-
-
-let taskArray = []; 
-localStorage.setItem('taskArray', JSON.stringify(e)); 
-let newArray = JSON.parse(localStorage.getItem('taskArray')); 
